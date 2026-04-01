683c4655e1b11cf5a5c52871f34e8daa29759f5e
Dinis Da Silva E Ferreira Da Conceicao <dinda-si@c1r1s11.42lisboa.com>
2026-04-01 17:55:55 +0100

    button for dice rolling

--- main.js
+++ main.js
@@ -13,18 +13,31 @@ camera.position.z = 5;
 const renderer = new THREE.WebGLRenderer({
 	canvas: document.getElementById('game-canvas')
 });
-renderer.getSize(window.innerWidth, window.innerHeight);
+renderer.setSize(window.innerWidth, window.innerHeight);
 
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000});
 const dice = new THREE.Mesh(geometry, material);
 scene.add(dice);
 
+let rolling = false;
+
 function animate() {
 	requestAnimationFrame(animate);
-	dice.rotation.x += 0.01;
-	dice.rotation.y += 0.01;
+	if (rolling) {
+		dice.rotation.x += 0.1;
+		dice.rotation.y += 0.1;
+	}
 	renderer.render(scene, camera);
 }
-
 animate();
+
+const button = document.createElement('button');
+button.innerText = 'Roll';
+button.addEventListener('click', () => {
+	rolling = true;
+	setTimeout(() => {
+		rolling = false;
+	}, 1000);
+});
+document.body.appendChild(button);
